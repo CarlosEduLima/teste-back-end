b1bbc3b74ee92cef25566a36fae39acd5c84c88e
Carlos Eduardo <carlos.lima@sysmanager.com.br>
2022-03-10 16:16:03 -0300

    start server

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+//const Routes = require('./main/config/routes-map.js')
+const cors = require("cors")
+dotenv.config()
+app.use(cors())
+//Routes(app)
+
+const mongoose = require("mongoose")
+
+mongoose
+  .connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((data) => console.log("connect!!", data))
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}.`)
+})
+
+module.exports = app
