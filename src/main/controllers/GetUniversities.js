c183e1b9dce52a3de72687b81374f84d7135dc6a
Carlos Eduardo <carlos.lima@sysmanager.com.br>
2022-03-11 09:49:22 -0300

    capitalize first country letter

--- src/main/controllers/GetUniversities.js
+++ src/main/controllers/GetUniversities.js
@@ -3,15 +3,17 @@ const UniversityDB = require("../../infra/MongoDB/queries/universityQueries")
 module.exports = {
   async GetUniversitiesController(httpResquest) {
     if (httpResquest.query.country) {
-      const query = await UniversityDB.getByCountry(httpResquest.query.country)
+      const country = httpResquest.query.country
+      country.charAt(0).toUpperCase() + country.slice(1);
+      const query = await UniversityDB.getByCountry(country)
       if (!query.success) {
-        return httpResponse.serverError()
+        return httpResponse.serverError(query.error)
       }
       return httpResponse.success(query.university)
     } else {
-      const query = await UniversityDB.getAll(20)
+      const query = await UniversityDB.getAll()
       if (!query.success) {
-        return httpResponse.serverError()
+        return httpResponse.serverError(query.error)
       }
       return httpResponse.success(query.universities)
     }
