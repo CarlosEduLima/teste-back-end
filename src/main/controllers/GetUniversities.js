5408b606d9146147c07f4e7c657710a9fa795a7e
Carlos Eduardo <carlos.lima@sysmanager.com.br>
2022-03-11 09:38:48 -0300

    create controller logic

--- src/main/controllers/GetUniversities.js
+++ src/main/controllers/GetUniversities.js
@@ -1,11 +1,19 @@
 const httpResponse = require("../helpers/httpResponse")
 const UniversityDB = require("../../infra/MongoDB/queries/universityQueries")
 module.exports = {
-  async GetUniversitiesController() {
-    const query = await UniversityDB.getAll(20)
-    if (!query.success) {
-      return httpResponse.serverError()
+  async GetUniversitiesController(httpResquest) {
+    if (httpResquest.query.country) {
+      const query = await UniversityDB.getByCountry(httpResquest.query.country)
+      if (!query.success) {
+        return httpResponse.serverError()
+      }
+      return httpResponse.success(query.university)
+    } else {
+      const query = await UniversityDB.getAll(20)
+      if (!query.success) {
+        return httpResponse.serverError()
+      }
+      return httpResponse.success(query.universities)
     }
-    return httpResponse.success(query.foundUniversities)
   },
 }
