e60d6dabf8e55b4a19aa6a41b7bf7ce390abbaa7
Carlos Eduardo <carlos.lima@sysmanager.com.br>
2022-03-11 00:11:05 -0300

    added routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
-//const Routes = require('./main/config/routes-map.js')
+const Routes = require("./main/config/routes-map.js")
 const cors = require("cors")
 dotenv.config()
 app.use(cors())
-//Routes(app)
+Routes(app)
 
 const mongoose = require("mongoose")
 
 mongoose
   .connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((data) => console.log("connect!!", data))
+  .then((data) => console.log("connect!!"))
 
 const PORT = process.env.PORT || 3001
 
