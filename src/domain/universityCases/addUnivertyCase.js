10bc2f7793e63a136aa388af32d9ec43499a8ce2
Carlos Eduardo <carlos.lima@sysmanager.com.br>
2022-03-11 22:35:57 -0300

    create addUniversityCase

---
+++ src/domain/universityCases/addUnivertyCase.js
@@ -0,0 +1,63 @@
+const httpResponse = require("../../main/helpers/httpResponse")
+const UniversityDb = require("../../infra/MongoDb/queries/universityQueries")
+module.exports = {
+  async AddUniversityCase(university) {
+    try {
+      if (!university) {
+        return {
+          success: false,
+          error: httpResponse.badRequest("University was not provided"),
+        }
+      }
+      const { name, country, alpha_two_code } = university
+      if (!name) {
+        return {
+          success: false,
+          error: HttpResponse.badRequest({
+            success: false,
+            msg: "name is required to create university",
+          }),
+        }
+      }
+      if (!country) {
+        return {
+          success: false,
+          error: HttpResponse.badRequest({
+            success: false,
+            msg: "country is required to create university",
+          }),
+        }
+      }
+      if (!alpha_two_code) {
+        return {
+          success: false,
+          error: HttpResponse.badRequest({
+            success: false,
+            msg: "alpha_two_code is required to create university",
+          }),
+        }
+      }
+      const result = await UniversityDb.getByName(name)
+      if (result.success && result.university) {
+        if (result.university.country === country) {
+          return {
+            success: false,
+            error: httpResponse.badRequest({
+              success: false,
+              msg: "University already exist",
+            }),
+          }
+        }
+      } else {
+        return {
+          success: true,
+        }
+      }
+    } catch (e) {
+      return {
+        success: false,
+        error: httpResponse.serverError(e),
+      }
+    }
+  },
+}
