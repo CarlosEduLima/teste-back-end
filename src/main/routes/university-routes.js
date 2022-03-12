f819ba6b05698d9eea15cff0472c92c402fcc907
Carlos Eduardo <carlos.lima@sysmanager.com.br>
2022-03-11 22:45:16 -0300

    add addUniversity router

--- src/main/routes/university-routes.js
+++ src/main/routes/university-routes.js
@@ -1,7 +1,9 @@
 const adpter = require("../config/express-router.adpter")
 const getUniversitiesRouter = require("../../presentation/routers/getUniversitiesRouter")
 const getUniversityRouter = require("../../presentation/routers/getUniversityRouter")
+const addUniversityRouter = require("../../presentation/routers/addUniversityRouter")
 module.exports = (router) => {
   router.get("/universities", adpter(getUniversitiesRouter()))
   router.get("/university/:id", adpter(getUniversityRouter()))
+  router.post("/universities", adpter(addUniversityRouter()))
 }
