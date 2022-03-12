92f756161f1283ce00f4d40251ba64441108e730
Carlos Eduardo <carlos.lima@sysmanager.com.br>
2022-03-12 14:48:26 -0300

    add university router

--- src/main/routes/university-routes.js
+++ src/main/routes/university-routes.js
@@ -3,9 +3,11 @@ const getUniversitiesRouter = require("../../presentation/routers/getUniversitie
 const getUniversityRouter = require("../../presentation/routers/getUniversityRouter")
 const addUniversityRouter = require("../../presentation/routers/addUniversityRouter")
 const updateUniversityRouter = require("../../presentation/routers/updateUniversityRouter")
+const deleteUniversityRouter = require("../../presentation/routers/deleteUniversityRouter")
 module.exports = (router) => {
   router.get("/universities", adpter(getUniversitiesRouter()))
   router.get("/university/:id", adpter(getUniversityRouter()))
   router.post("/universities", adpter(addUniversityRouter()))
   router.put("/universities/:id", adpter(updateUniversityRouter()))
+  router.delete("/university/:id", adpter(deleteUniversityRouter()))
 }
