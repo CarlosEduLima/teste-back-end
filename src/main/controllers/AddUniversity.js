c096b48b276e4bc17b48b09356efee2296a7f04e
Carlos Eduardo <carlos.lima@sysmanager.com.br>
2022-03-12 14:27:48 -0300

    rename validateUser to validadeUniversity

--- src/main/controllers/AddUniversity.js
+++ src/main/controllers/AddUniversity.js
@@ -1,16 +1,16 @@
 const httpResponse = require("../../main/helpers/httpResponse")
 const {
   AddUniversityCase,
-} = require("../../domain/universityCases/addUnivertyCase")
+} = require("../../domain/universityCases/addUniversityCase")
 const UniversityDB = require("../../infra/MongoDb/queries/universityQueries")
 
 module.exports = {
   async AddUniversityController(httpRequest) {
     const university = httpRequest.body
 
-    const validateUser = await AddUniversityCase(university)
-    if (!validateUser.success) {
-      return validateUser.error
+    const validateUniversity = await AddUniversityCase(university)
+    if (!validateUniversity.success) {
+      return validateUniversity.error
     }
     const createdUniversity = await UniversityDB.create(university)
     if (!createdUniversity.success) {
